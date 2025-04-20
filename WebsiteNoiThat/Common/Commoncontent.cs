18cb7d6266acab2919d2b4409772c91e74e3009d
maivu1 <maivu00174@gmail.com>
2025-04-20 10:21:52 +0700

    Login

---
+++ WebsiteNoiThat/Common/Commoncontent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteNoiThat.Common
+{
+    public static  class Commoncontent
+    {
+        public static string user_sesion="user_session";
+        public static string user_sesion_admin = "user_session_admin";
+       
+    }
+}
---
+++ WebsiteNoiThat/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteNoiThat.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
